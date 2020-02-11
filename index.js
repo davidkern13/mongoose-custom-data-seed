f1a9184cf1f41e78708c6c98e2beec4b42444b60
David Kern <david33belle@gmail.com>
2020-02-11 17:05:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,18 @@ const seed = (data = {}, schema, options = '') => {
     }
 };
 
+/**
+ * createSeeder function - post all data in to schema
+ * @function createSeeder
+ * @param {data} postData - Array of objects to post into schema
+ * @param {schema} schema - Schema of mongoose db
+ * @returns {Promise}
+ */
 const createSeeder = (data, schema) => {
 
-    return new Promise(async (resolve, reject) => {
+    return new Promise( (resolve, reject) => {
         try {
-            const saveData = await schema.insertMany(data);
+            const saveData =  schema.insertMany(data);
             resolve(saveData);
         } catch (e) {
             reject('schema reject -> ' + e)
@@ -33,11 +40,17 @@ const createSeeder = (data, schema) => {
     });
 }
 
+/**
+ * dropDatabase function - remove all data from schema
+ * @function dropDatabase
+ * @param {schema} schema - Schema of mongoose db
+ * @returns {Promise}
+ */
 const dropDatabase = (schema) => {
 
-    return new Promise(async (resolve, reject) => {
+    return new Promise( (resolve, reject) => {
         try {
-            const tableData = await schema.remove({});
+            const tableData = schema.remove({});
             resolve(tableData);
         } catch (e) {
             reject('drop table reject -> ' + e)
